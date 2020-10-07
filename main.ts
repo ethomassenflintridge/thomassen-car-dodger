32ee183a4c4038f0df8a2cfb41d6fe82e919897b
ethomassenflintridge <72520353+ethomassenflintridge@users.noreply.github.com>
2020-10-07 11:09:59 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,51 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.ay = 500
+    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
+        mySprite.vy = -600
+    }
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    game.over(false)
+})
+let projectile: Sprite = null
+let mySprite: Sprite = null
+tiles.setTilemap(tilemap`level`)
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . 
+    e e e . . . . e e e . . . . 
+    c d d c . . c d d c . . . . 
+    c b d d f f d d b c . . . . 
+    c 3 b d d b d b 3 c . . . . 
+    f b 3 d d d d 3 b f . . . . 
+    e d d d d d d d d e . . . . 
+    e d f d d d d f d e b f b . 
+    f d d f d d f d d f f d f . 
+    f b d d 3 3 d d 9 b f d f . 
+    . f 9 9 9 9 9 9 d b d b f . 
+    . f d d d d d d d f f f . . 
+    . f d b d f f f d f . . . . 
+    . . f f f f . . f f . . . . 
+    `, SpriteKind.Player)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(1, 5))
+game.onUpdateInterval(2000, function () {
+    projectile = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 6 6 6 6 6 6 6 6 . . 
+        . . . . . 6 c 6 6 6 6 6 6 9 6 . 
+        . . . . 6 c c 6 6 6 6 6 6 9 c 6 
+        . . d 6 9 c c 6 9 9 9 9 9 9 c c 
+        . d 6 6 9 c b 8 8 8 8 8 8 8 6 c 
+        . 6 6 6 9 b 8 8 b b b 8 b b 8 6 
+        . 6 6 6 6 6 8 b b b b 8 b b b 8 
+        . 6 6 6 6 8 6 6 6 6 6 8 6 6 6 8 
+        . 6 d d 6 8 f 8 8 8 f 8 8 8 8 8 
+        . d d 6 8 8 8 f 8 8 f 8 8 8 8 8 
+        . 8 8 8 8 8 8 8 f f f 8 8 8 8 8 
+        . 8 8 8 8 f f f 8 8 8 8 f f f f 
+        . . . 8 f f f f f 8 8 f f f f f 
+        . . . . f f f f . . . . f f f . 
+        . . . . . . . . . . . . . . . . 
+        `, randint(-100, -80), 0)
+    tiles.placeOnTile(projectile, tiles.getTileLocation(9, 5))
+    info.changeScoreBy(1)
+})
---
+++ tilemap.g.ts
@@ -0,0 +1,23 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers.registerTilemapFactory(function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level": return tiles.createTilemap(hex`0a0008000402020202020202020305060606060707070707050606060607070707070506060606070707070705060606060707070707050808080809090909090101010101010101010101010101010101010101`, img`
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+. . . . . . . . . . 
+2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.dungeon.floorLight2,sprites.dungeon.purpleOuterNorth0,sprites.dungeon.purpleOuterNorthEast,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleOuterWest0,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.purpleInnerNorthEast,sprites.dungeon.purpleInnerSouthWest,sprites.dungeon.purpleInnerSouthEast], TileScale.Sixteen)
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
